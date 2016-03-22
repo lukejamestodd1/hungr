c4efe0b98e44a4da887dab7f465e7e9bbcda39d8
Luke Todd <lukejamestodd1@gmail.com>
2016-03-22 11:57:29 +1100

    api address

--- app/assets/javascripts/spaModels.js
+++ app/assets/javascripts/spaModels.js
@@ -1,18 +1,18 @@
 //================= MODELS & COLLECTIONS =================//
 var Status = Backbone.Model.extend({
-	urlRoot: 'http://hungrapp.herokuapp.com/statuses'
+	urlRoot: 'https://hungrapp.herokuapp.com/statuses'
 });
 
 var User = Backbone.Model.extend({
-	urlRoot: 'http://hungrapp.herokuapp.com/users'
+	urlRoot: 'https://hungrapp.herokuapp.com/users'
 });
 
 var Statuses = Backbone.Collection.extend({
 	model: Status,
-	url: 'http://hungrapp.herokuapp.com/statuses'
+	url: 'https://hungrapp.herokuapp.com/statuses'
 });
 
 var Users = Backbone.Collection.extend({
 	model: User,
-	url: 'http://hungrapp.herokuapp.com/users'
+	url: 'https://hungrapp.herokuapp.com/users'
 });

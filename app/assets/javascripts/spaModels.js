fc46abe6fde654505079a6af97ea9bb9989aa112
Luke Todd <lukejamestodd1@gmail.com>
2016-03-23 12:13:32 +1100

    fixing menu

--- app/assets/javascripts/spaModels.js
+++ app/assets/javascripts/spaModels.js
@@ -1,18 +1,39 @@
-//================= MODELS & COLLECTIONS =================//
+// //================= MODELS & COLLECTIONS =================//
+// var Status = Backbone.Model.extend({
+// 	urlRoot: 'https://hungrapp.herokuapp.com/statuses'
+// });
+
+// var User = Backbone.Model.extend({
+// 	urlRoot: 'https://hungrapp.herokuapp.com/users'
+// });
+
+// var Statuses = Backbone.Collection.extend({
+// 	model: Status,
+// 	url: 'https://hungrapp.herokuapp.com/statuses'
+// });
+
+// var Users = Backbone.Collection.extend({
+// 	model: User,
+// 	url: 'https://hungrapp.herokuapp.com/users'
+// });
+
+
+//================= LOCAL HOST FOR DEVELOPMENT =================//
 var Status = Backbone.Model.extend({
-	urlRoot: 'https://hungrapp.herokuapp.com/statuses'
+	urlRoot: 'http://localhost:3000/statuses'
 });
 
 var User = Backbone.Model.extend({
-	urlRoot: 'https://hungrapp.herokuapp.com/users'
+	urlRoot: 'http://localhost:3000/users'
 });
 
 var Statuses = Backbone.Collection.extend({
 	model: Status,
-	url: 'https://hungrapp.herokuapp.com/statuses'
+	url: 'http://localhost:3000/statuses'
 });
 
 var Users = Backbone.Collection.extend({
 	model: User,
-	url: 'https://hungrapp.herokuapp.com/users'
+	url: 'http://localhost:3000/users'
 });
+

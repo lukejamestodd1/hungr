9988f4d09146be28ba1ff3020fa34690d6783375
Luke Todd <lukejamestodd1@gmail.com>
2016-03-29 17:42:01 +1100

    deploy mode

--- app/assets/javascripts/spaModels.js
+++ app/assets/javascripts/spaModels.js
@@ -1,39 +1,39 @@
-// //================= MODELS & COLLECTIONS =================//
-// var Status = Backbone.Model.extend({
-// 	urlRoot: 'https://hungrapp.herokuapp.com/statuses'
-// });
-
-// var User = Backbone.Model.extend({
-// 	urlRoot: 'https://hungrapp.herokuapp.com/users'
-// });
-
-// var Statuses = Backbone.Collection.extend({
-// 	model: Status,
-// 	url: 'https://hungrapp.herokuapp.com/statuses'
-// });
-
-// var Users = Backbone.Collection.extend({
-// 	model: User,
-// 	url: 'https://hungrapp.herokuapp.com/users'
-// });
-
-
-//================= LOCAL HOST FOR DEVELOPMENT =================//
+//================= MODELS & COLLECTIONS =================//
 var Status = Backbone.Model.extend({
-	urlRoot: 'http://localhost:3000/statuses'
+	urlRoot: 'https://hungrapp.herokuapp.com/statuses'
 });
 
 var User = Backbone.Model.extend({
-	urlRoot: 'http://localhost:3000/users'
+	urlRoot: 'https://hungrapp.herokuapp.com/users'
 });
 
 var Statuses = Backbone.Collection.extend({
 	model: Status,
-	url: 'http://localhost:3000/statuses'
+	url: 'https://hungrapp.herokuapp.com/statuses'
 });
 
 var Users = Backbone.Collection.extend({
 	model: User,
-	url: 'http://localhost:3000/users'
+	url: 'https://hungrapp.herokuapp.com/users'
 });
+
+
+// //================= LOCAL HOST FOR DEVELOPMENT =================//
+// var Status = Backbone.Model.extend({
+// 	urlRoot: 'http://localhost:3000/statuses'
+// });
+
+// var User = Backbone.Model.extend({
+// 	urlRoot: 'http://localhost:3000/users'
+// });
+
+// var Statuses = Backbone.Collection.extend({
+// 	model: Status,
+// 	url: 'http://localhost:3000/statuses'
+// });
+
+// var Users = Backbone.Collection.extend({
+// 	model: User,
+// 	url: 'http://localhost:3000/users'
+// });
 
